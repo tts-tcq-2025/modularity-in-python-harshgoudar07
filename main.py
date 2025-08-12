58dc134b0ef1416463de079b95668c7a489ee188
harsh@07 <59923508+harshgoudar07@users.noreply.github.com>
2025-08-12 15:00:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,18 +29,15 @@ def get_pair_number_from_color(major_color, minor_color):
     raise Exception('Minor index out of range')
   return major_index * len(MINOR_COLORS) + minor_index + 1
 
-
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
   major_color, minor_color = get_color_from_pair_number(pair_number)
   assert(major_color == expected_major_color)
   assert(minor_color == expected_minor_color)
 
-
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
-
 
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
